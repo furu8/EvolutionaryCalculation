2214d8d47c5e5c39ccde08c816c4f931ed9ffeb3
furu8 <foooolurk1125@gmail.com>
2020-07-18 16:49:06 +0900

    de完成

--- 0714/de.py
+++ 0714/de.py
@@ -3,48 +3,69 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sys
 
-def sphere_func(D, X, i):
+def init_fx(M, D, X, fx):
+    f = np.zeros(M)
+    
+    if fx == 'sphere':
+        for m in range(M):
+            ans = 0
+            for d in range(D):
+                Xd = X[m][d]
+                ans += Xd ** 2
+            f[m] = ans
+    elif fx == 'rastrigin':
+        for m in range(M):
+            ans = 0
+            for d in range(D):
+                Xd = X[m][d]
+                ans += ((Xd**2) - 10*np.cos(2*np.pi*Xd) + 10)
+            f[m] = ans
+    else:
+        print('引数fxが間違っています')
+
+    return f
+
+def sphere_func(D, U):
     ans = 0
     for d in range(D):
-        Xd = X[i][d]
+        Xd = U[d]
         ans += Xd ** 2
     return ans
 
-def rastringin_func(D, X, i):
+def rastringin_func(D, U):
     ans = 0
     for d in range(D):
-        Xd = X[i][d]
+        Xd = U[d]
         ans += ((Xd**2) - 10*np.cos(2*np.pi*Xd) + 10)
     return ans
 
-def main(M, D, c, w, fx):
+def main(M, D, c, Fw, fx):
     Tmax = 1000          # 最大繰り返し回数
     Cr = 0.9             # 終了条件
     x_min, x_max = -5, 5 # 範囲
 
     X = (x_max - x_min) * np.random.rand(M,D) + x_min # 位置
-    Xnew = (x_max - x_min) * np.random.rand(M,D) + x_min # 位置
-    V = np.zeros((M,D)) # 速度
-    U = np.zeros(d)     # 解候補
+    Xnew = np.zeros((M,D)) 
+    V = np.zeros(D) # 速度
+    U = np.zeros(D)     # 解候補
     F = np.zeros(M)     # 評価関数を格納
     Ftmp = 0     # 評価関数を格納
     Fend = 1e-5 # 終了条件
-    Fbest = np.full(M, float('inf'))      # pbest
-    Xbest = np.full((M, D), float('inf'))
-
+    Fbest = float('inf')
+    Xbest = np.full(D, float('inf'))
     cnvg_plot = []
+
+    # 初期値によるF = f(x^0ベクトル) の計算
+    F = init_fx(M, D, X, fx)
 
     for t in range(1, Tmax+1):
         for i in range(M):
-            for j in range(D):
-                # 3個体選ぶ
-                
-                # Vを作成
-                
-                pass
-            
-            Jr = np.random.rand()
-
+            # 3個体選ぶ
+            a, b, c = np.random.choice(np.arange(M), 3, replace=False)
+            V = X[a] + Fw * (X[b] - X[c])
+            # 突然変異
+            Jr = np.random.randint(D)
+            # 交叉
             for j in range(D):
                 ri = np.random.rand()
                 if ri < Cr or j == Jr:
@@ -53,12 +74,12 @@ def main(M, D, c, w, fx):
                     U[j] = X[i][j]
             
             if fx == 'sphere':
-                Ftmp = sphere_func(D, X, i)
-            elif fx == 'rastringin':
-                Ftmp = rastringin_func(D, X, i)
+                Ftmp = sphere_func(D, U)
+            elif fx == 'rastrigin':
+                Ftmp = rastringin_func(D, U)
             else:
                 print('引数fxが間違っています')
-
+           
             if Ftmp < F[i]:
                 F[i] = Ftmp
                 # XnewをUで上書き
@@ -68,23 +89,25 @@ def main(M, D, c, w, fx):
                 Xbest = X[i]
             else:
                 # XnewをXで上書き
-                Xnew[i] = X
+                Xnew[i] = X[i]
 
+        # XをXnewで上書き     
+        X = Xnew
         if Fbest < Fend:
-            cnvg_plot.append([t, Fg])
+            cnvg_plot.append([t, Fbest])
             break
            
     # print("終了時刻t={}".format(t))
-    # print("解の目的関数値Fg={}".format(Fg))
-    # print("解Xg={}".format(Xg))
-    return t, Fg, cnvg_plot
+    # print("解の目的関数値Fbest={}".format(Fbest))
+
+    return t, Fbest, cnvg_plot
 
 if __name__ == "__main__":
     M = 30                # 個体数
-    D_list = [2]     # 解の次元
+    D_list = [20]   # 解の次元
     c = 0.9               # DEのパラメータ
     Fw = 0.5              # DEのパラメータ
-    fx_list = ['sphere', 'rastringin']
+    fx_list = ['sphere', 'rastrigin']
 
     time_list = np.array([])
     fg_list = np.array([])
@@ -105,11 +128,14 @@ if __name__ == "__main__":
                 time_list = np.append(time_list,t)
                 fg_list = np.append(fg_list,Fg)
                 cnvg_plot_list.append(cnvg)
+                break
             ans_d_list.append(d)
             ans_fx_list.append(fx)
             ans_fg_mean_list.append(fg_list.mean())
             ans_fg_var_list.append(fg_list.var())
             ans_time_mean_list.append(time_list.mean())
+         
+        print("解の目的関数値Fg={}".format(ans_fg_mean_list))
 
 
     df['d'] = ans_d_list
@@ -118,6 +144,6 @@ if __name__ == "__main__":
     df['fg_var'] = ['{:.3e}'.format(v) for v in ans_fg_var_list]
     df['time_mean'] = ans_time_mean_list
 
-    # df.to_csv('pso.csv')
+    # df.to_csv('de.csv')
     print(df)
         
