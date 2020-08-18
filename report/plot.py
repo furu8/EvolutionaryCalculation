c167530716f9e2a58c6cd570b60dfb057a685fa6
furu8 <foooolurk1125@gmail.com>
2020-08-19 01:13:43 +0900

    result png

--- report/plot.py
+++ report/plot.py
@@ -7,11 +7,15 @@ def read_df(path):
     df = pd.read_csv(path)
     return df
 
-def plot_graph(pso_df, de_df):
-    plt.plot(pso_df['t'], np.log(pso_df['fg']))
-    plt.plot(de_df['t'], np.log(de_df['fg']))
+def plot_graph(name, pso_df, de_df):
+    plt.title(name)
+    plt.plot(pso_df['t'], np.log(pso_df['fg']), label='pso')
+    plt.plot(de_df['t'], np.log(de_df['fg']), label='de')
     plt.xlim(-1, 1001)
-    plt.ylim(-10, 10)
+    plt.ylim(-12, 7)
+    plt.legend(loc='lower right')
+    # plt.show()
+    plt.savefig(name)
     plt.show()
 
 def main():
@@ -27,10 +31,10 @@ def main():
     de_rastrigin2_df = read_df('de_rastrigin2.csv')
     de_rastrigin20_df = read_df('de_rastrigin20.csv')
 
-    plot_graph(pso_sphere2_df, de_sphere2_df)
-    plot_graph(pso_sphere20_df, de_sphere20_df)
-    plot_graph(pso_rastrigin2_df, de_rastrigin2_df)
-    plot_graph(pso_rastrigin20_df, de_rastrigin20_df)
+    plot_graph('sphere2',pso_sphere2_df, de_sphere2_df)
+    plot_graph('sphere20',pso_sphere20_df, de_sphere20_df)
+    plot_graph('rastrigin2',pso_rastrigin2_df, de_rastrigin2_df)
+    plot_graph('rastrigin20',pso_rastrigin20_df, de_rastrigin20_df)
 
 if __name__ == "__main__":
     main()
